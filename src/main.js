f26e1fef59af55a1c23f1bc78c6d7014d765eb4e
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-09-14 08:19:44 +0200

    add-modules

---
+++ src/main.js
@@ -0,0 +1,5 @@
+/*Підключення додаткових модулів*/
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
