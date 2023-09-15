a54819ad9884864ffc0073a4b5e8c36cff3c94a2
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2023-09-16 01:00:18 +0300

    готовий запит на бек по івентам

--- src/js/API/events-api.js
+++ src/js/API/events-api.js
@@ -1,42 +1,37 @@
-function featchEvents() {
-  return fetch(`https://tasty-treats-backend.p.goit.global/api/events`).then(
-    resp => {
-      if (!resp.ok) {
-        throw new Error(`eror`);
-      }
-      return resp.json();
-    }
-  );
-}
-
-const swiperSlide = document.querySelector('.hero-swiper-slide');
-
 function getEvents(slide) {
-  const markup = slide.map(
-    ({
-      cook: { imgWebpUrl: cookImgWebp, imgUrl: cookImgUrl },
-      topic: {
-        imgWebpUrl: topicImgWebp,
-        imgUrl: topicImgUrl,
-        previewWebpUrl,
-        previewUrl,
-        name,
-        area,
-      },
-    }) => {
-      return `<div class="hero-swiper-slide">
-                        <div class="chef">
+  fetch('https://tasty-treats-backend.p.goit.global/api/events')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const slides = data.map(
+        ({
+          cook: { imgWebpUrl: cookImgWebp, imgUrl: cookImgUrl },
+          topic: {
+            imgWebpUrl: topicImgWebp,
+            imgUrl: topicImgUrl,
+            previewWebpUrl,
+            previewUrl,
+            name,
+            area,
+          },
+        }) => {
+          return `
+                       <div class="chef">
                             <picture>
                                 <source srcset="${cookImgWebp}" type="img/webp" />
                                 <source srcset="${cookImgUrl}" type="img/png" />
-                                <img class="chef-image" src="../img/chief1.png" alt="">
+                                <img class="chef-image" src="${cookImgUrl}" alt="">
                             </picture>
                         </div>
                         <div class="hero-preview">
                             <picture>
                                 <source srcset="${previewWebpUrl}" type="img/webp" />
                                 <source srcset="${previewUrl}" type="img/png" />
-                                <img class="preview-image" src="../img/preview1.png" alt="">
+                                <img class="preview-image" src="${previewUrl}" alt="">
                             </picture>
                             <h2 class="hero-swiper-title">${name}</h2>
                             <p class="hero-swiper-desc">${area}</p>
@@ -45,33 +40,18 @@ function getEvents(slide) {
                             <picture>
                                 <source srcset="${topicImgWebp}" type="img/webp" />
                                 <source srcset="${topicImgUrl}" type="img/png" />
-                                <img class="dish-image" src="../img/dish1.png" alt="">
+                                <img class="dish-image" src="${topicImgUrl}" alt="">
                             </picture>
                         </div>
-                    </div>`.join('');
-    }
-  );
-  swiperSlide.insertAdjacentHTML('beforeend', markup);
+                    `;
+        }
+      );
+
+      slide.insertAdjacentHTML('beforeend', slides.join(''));
+    })
+    .catch(error => {
+      console.error('Виникла проблема з операцією запиту:', error);
+    });
 }
 
-// import Swiper from 'swiper';
-// // import Swiper styles
-// import 'swiper/css';
-// import 'swiper/css';
-// import 'swiper/css/pagination';
-
-// // ініцілізація слайдеру
-
-// const hero_slider = new Swiper(`.js-hero-slider`, {
-//   direction: 'vertical',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Default parameters
-//   slidesPerView: 1,
-//   spaceBetween: 16,
-// });
+getEvents(document.querySelector('.hero-swiper-slide'));
