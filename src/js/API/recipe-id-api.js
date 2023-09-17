f6dc449ff7bf6fa015b0ae83f35d141ddb607df5
Ira-Se <129228693+Ira-Se@users.noreply.github.com>
2023-09-17 23:39:24 +0300

    fixes after review

--- src/js/API/recipe-id-api.js
+++ src/js/API/recipe-id-api.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 export async function fetchRecipeByID(id) {
   try {
     const resp = await axios.get(
-      'https://tasty-treats-backend.p.goit.global/api/recipes/{id}'
+      'https://tasty-treats-backend.p.goit.global/api/recipes/${id}'
     );
     return resp.data;
   } catch (error) {
-    console.error('Error:', error);
+    throw error;
   }
 }
