ea8a71f7a9fe3ebada355c9bc1b2686dc393598e
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-09-17 15:03:12 +0300

    add functions for submit form and API request

---
+++ src/js/API/order-api.js
@@ -0,0 +1,21 @@
+
+import axios from 'axios';
+
+export async function submitForm(userName, phone, email, comment) {
+    try {
+        const {data} = await axios.post('https://tasty-treats-backend.p.goit.global/api/orders', {
+            name: userName,
+            phone: phone,
+            email: email,
+            comment: comment
+          }, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+          }
+        )
+       console.log(data);
+    } catch (error) {
+      throw error;
+    }
+  }
---
+++ src/js/actions/modal-new-order.js
@@ -0,0 +1,43 @@
+import { Notify } from 'notiflix';
+import { submitForm } from 'js/API/order-api';
+
+const cartBtn = document.querySelector('.order-icon');
+const closeModalBtn = document.querySelector('.modal-close-btn');
+const modalWindow = document.querySelector('.modal-overlay');
+const form = document.querySelector('.form');
+
+cartBtn.addEventListener('click', handlerClick);
+closeModalBtn.addEventListener('click', closeModal);
+form.addEventListener('submit', onSubmit);
+
+// open modal
+function handlerClick() {
+  modalWindow.classList.remove('visually-hidden');
+}
+
+// close modal
+function closeModal() {
+  modalWindow.classList.add('visually-hidden');
+}
+
+// function submit data of form
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const userName = evt.currentTarget.elements.modal_name.value;
+  const phone = evt.currentTarget.elements.modal_phone.value;
+  const email = evt.currentTarget.elements.modal_email.value;
+  const comment = evt.currentTarget.elements.modal_comment.value;
+
+  if (userName == '' || phone == '' || email == '') {
+    Notify.failure('Ensure you input a value in both fields!');
+  } else {
+    Notify.success('This form has been successfully submitted!');
+
+    submitForm(userName, phone, email, comment);
+    evt.currentTarget.reset();
+    closeModal();
+  }
+}
+
+export { handlerClick, closeModal };
