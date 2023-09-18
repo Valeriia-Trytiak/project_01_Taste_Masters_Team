e554abf68075577cd17880a8f965f63c804161a1
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-09-18 17:40:13 +0300

    Small fixing, theme logic

--- src/js/actions/header.js
+++ src/js/actions/header.js
@@ -3,14 +3,11 @@ const closeMobileMenuElement = document.querySelector('.mob-menu-close-btn');
 const mobileMenuElement = document.querySelector('.mobile-menu');
 const menuLinkHomeElement = document.querySelector('.link-home');
 const menuLinkFavElement = document.querySelector('.link-fav');
+const mobMenuLinkHomeElement = document.querySelector('.mob-menu-link-home');
+const mobMenuLinkFavElement = document.querySelector('.mob-menu-link-fav');
 
 menuLinkFavElement.addEventListener('click', handlerMenuLink);
-
-export function handlerMenuLink () {
-    menuLinkFavElement.classList.add('current');
-    menuLinkHomeElement.classList.remove('current');
-}
-
+mobMenuLinkFavElement.addEventListener('click', handlerMobMenuLink);
 
 openMobileMenuElement.addEventListener('click', openMobileMenu);
 closeMobileMenuElement.addEventListener('click', closeMobileMenu);
@@ -25,5 +22,67 @@ export function closeMobileMenu () {
     mobileMenuElement.classList.add('visually-hidden');
 }
 
+export function handlerMenuLink () {
+    menuLinkFavElement.classList.add('current');
+    menuLinkHomeElement.classList.remove('current');
+}
 
+export function handlerMobMenuLink () {
+    mobMenuLinkFavElement.classList.add('active-mob-nav');
+    mobMenuLinkHomeElement.classList.remove('active-mob-nav');
+}
+
+
+//  =========== change the theme ===========
+
+window.addEventListener("load", windowLoad);
+
+export function windowLoad () {
+    
+	const htmlBlock = document.documentElement;
+
+	
+	const saveUserTheme = localStorage.getItem('user-theme');
+
+    
+	const themeButton = document.querySelector('.switch__input');
+    const mobileThemeButton = document.querySelector('.switch__input__mob');
+	if (themeButton) {
+		themeButton.addEventListener("click", function (e) {
+			changeTheme(true);
+		});
+	}
+    if (mobileThemeButton) {
+        mobileThemeButton.addEventListener("click", function (e) {
+			changeTheme(true);
+		});
+    }
+
+    
+	function setThemeClass() {
+		if (saveUserTheme) {
+			htmlBlock.classList.add(saveUserTheme)
+		} else {
+			htmlBlock.classList.add(userTheme);
+		}
+	}
+
+	
+	setThemeClass();
+
+
+	function changeTheme(saveTheme = false) {
+		let currentTheme = htmlBlock.classList.contains('dark') ? 'dark' : 'light';
+		let newTheme;
+
+		if (currentTheme === 'light') {
+			newTheme = 'dark';
+		} else if (currentTheme === 'dark') {
+			newTheme = 'light';
+		}
+		htmlBlock.classList.remove(currentTheme);
+		htmlBlock.classList.add(newTheme);
+		saveTheme ? localStorage.setItem('user-theme', newTheme) : null;
+	}
+}
 
