d401a34308ae2ed9338455e9df52518da33cf8ac
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-09-18 21:54:30 +0200

    Update markup-card.js

--- src/js/markup/markup-card.js
+++ src/js/markup/markup-card.js
@@ -3,7 +3,7 @@ export function createMarkupCard(array) {
     url: 'https://st2.depositphotos.com/4410397/7644/v/450/depositphotos_76444781-stock-illustration-valentines-day-meal-line-icon.jpg',
     text: 'Not Found',
   };
-    
+
   return array
     .map(
       ({ _id, title, description, preview, rating }) => `
@@ -17,12 +17,12 @@ export function createMarkupCard(array) {
          <div class="card-content">
           <button class="btn-heart" type="button">
             <svg width="22" height="22">
-              <use href="./img/icons.svg#heart-inactive"></use>
+              <use href="/img/icons.svg#heart-inactive"></use>
             </svg>
           </button>
           <button class="btn-heart visually-hidden" type="button">
             <svg width="22" height="22">
-              <use href="./img/icons.svg#heart-active"></use>
+              <use href="/img/icons.svg#heart-active"></use>
             </svg>
           </button>
           <h3 class="card-title">${title || defaults.text}</h3>
@@ -35,27 +35,27 @@ export function createMarkupCard(array) {
               <ul class="rating-stars-list js-rating-stars-list">
                 <li>
                   <svg class="rating-star-icon" width="18" height="18">
-                    <use href="./img/icons.svg#star"></use>
+                    <use href="/img/icons.svg#star"></use>
                   </svg>
                 </li>
                 <li>
                   <svg class="rating-star-icon" width="18" height="18">
-                    <use href="./img/icons.svg#star"></use>
+                    <use href="/img/icons.svg#star"></use>
                   </svg>
                 </li>
                 <li>
                   <svg class="rating-star-icon" width="18" height="18">
-                    <use href="./img/icons.svg#star"></use>
+                    <use href="/img/icons.svg#star"></use>
                   </svg>
                 </li>
                 <li>
                   <svg class="rating-star-icon" width="18" height="18">
-                    <use href="./img/icons.svg#star"></use>
+                    <use href="/img/icons.svg#star"></use>
                   </svg>
                 </li>
                 <li>
                   <svg class="rating-star-icon" width="18" height="18">
-                    <use href="./img/icons.svg#star"></use>
+                    <use href="/img/icons.svg#star"></use>
                   </svg>
                 </li>
               </ul>
