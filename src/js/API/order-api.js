bbedf843d862d4d638d410c98ba953e6959252c1
sofimadryha <75246511+Sofia-Madryha@users.noreply.github.com>
2023-09-18 23:32:21 +0300

    fix api request and change pattern for phone num

--- src/js/API/order-api.js
+++ src/js/API/order-api.js
@@ -1,9 +1,8 @@
-
 import axios from 'axios';
 
 export async function addNewOrder(userName, phone, email, comment) {
     try {
-        const {data} = await axios.post('https://tasty-treats-backend.p.goit.global/api/orders', JSON.stringify( {
+        const {data} = await axios.post('https://tasty-treats-backend.p.goit.global/api/orders/add', JSON.stringify( {
             name: userName,
             phone: phone,
             email: email,
