31596a94aa9d727ad782915d91133c74dbfd42be
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-09-19 00:24:51 +0200

    update

--- src/js/actions/pagination-home.js
+++ src/js/actions/pagination-home.js
@@ -16,10 +16,10 @@ export async function initializePagination() {
   async function calculateTotalPages() {
     const response = await serviceAllRecipes(perPage, currentPage);
     console.log('API Response:', response.data);
-  
+
     const totalPages = response.data.totalPages;
     console.log('Total Pages:', totalPages);
-  
+
     return totalPages;
   }
 
@@ -35,9 +35,22 @@ export async function initializePagination() {
       btn.disabled = currentPage === 1;
     });
 
+    // Inside the updatePageButtons function
     nextBtns.forEach((btn) => {
       btn.disabled = currentPage === totalPages;
+      if (currentPage === totalPages) {
+        btn.classList.add('last-page'); // Add the class on the last page
+      } else {
+        btn.classList.remove('last-page'); // Remove the class on other pages
+      }
     });
+
+    document.querySelector('.last-page-btn').disabled = currentPage === totalPages;
+    if (currentPage === totalPages) {
+      document.querySelector('.last-page-btn').classList.add('last-page'); // Add the class on the last page
+    } else {
+      document.querySelector('.last-page-btn').classList.remove('last-page'); // Remove the class on other pages
+    }
 
     const maxPagesToShow = 3;
     let startPage = currentPage - 1;
@@ -73,32 +86,104 @@ export async function initializePagination() {
     return parseInt(elemToPage) || 6; // Default to 6 if custom property is not set
   }
 
-  // Event listeners for previous page buttons
-  prevBtns.forEach((btn) => {
+// Event listeners for previous page button
+prevBtns.forEach((btn) => {
     btn.addEventListener('click', async () => {
       if (currentPage > 1) {
         currentPage--;
         await updatePage();
+  
+        // Remove the current-page class from all page buttons
+        pageButtons.forEach((button) => {
+          button.classList.remove('current-page');
+        });
+  
+        // Add the current-page class to the current page button
+        document.querySelector(`.pages-btn:nth-child(${currentPage})`).classList.add('current-page');
+  
+        // Update styles for first-page-btn and prev-btn
+        if (currentPage === 1) {
+          document.querySelector('.first-page-btn').style.backgroundColor = 'var(--cl-number-pages)';
+          document.querySelector('.first-page-btn').style.borderColor = 'var(--txt-cl-30-light-theme)';
+          prevBtns.forEach((prev) => {
+            prev.style.backgroundColor = 'var(--cl-number-pages)';
+            prev.style.borderColor = 'var(--txt-cl-30-light-theme)';
+          });
+        } else {
+          document.querySelector('.first-page-btn').style.backgroundColor = 'var(--accent-cl)';
+          document.querySelector('.first-page-btn').style.borderColor = 'var(--accent-cl)';
+          prevBtns.forEach((prev) => {
+            prev.style.backgroundColor = 'var(--accent-cl)';
+            prev.style.borderColor = 'var(--accent-cl)';
+          });
+        }
       }
     });
   });
-
-  // Event listeners for next page buttons
+  
+  // Event listeners for next page button
   nextBtns.forEach((btn) => {
     btn.addEventListener('click', async () => {
       const totalPages = await calculateTotalPages();
       if (currentPage < totalPages) {
         currentPage++;
         await updatePage();
+  
+        // Remove the current-page class from all page buttons
+        pageButtons.forEach((button) => {
+          button.classList.remove('current-page');
+        });
+  
+        // Add the current-page class to the current page button
+        document.querySelector(`.pages-btn:nth-child(${currentPage})`).classList.add('current-page');
+  
+        // Update styles for last-page-btn and next-btn
+        if (currentPage === totalPages) {
+          document.querySelector('.last-page-btn').style.backgroundColor = 'var(--cl-number-pages)';
+          nextBtns.forEach((next) => {
+            next.style.backgroundColor = 'var(--cl-number-pages)';
+          });
+        } else {
+          document.querySelector('.last-page-btn').style.backgroundColor = 'var(--accent-cl)';
+          nextBtns.forEach((next) => {
+            next.style.backgroundColor = 'var(--accent-cl)';
+          });
+        }
       }
     });
   });
-
+  
   // Event listener for the "..." (dots) button
   dotsBtn.addEventListener('click', async () => {
     const totalPages = await calculateTotalPages();
     if (currentPage < totalPages) {
       currentPage = Math.min(currentPage + 3, totalPages);
+      await updatePage();
+    }
+  });
+
+  // Event listener for first page button
+  document.querySelector('.first-page-btn').addEventListener('click', async () => {
+    if (currentPage !== 1) {
+      // Check if it's not already on the first page
+      currentPage = 1;
+      await updatePage();
+
+      // Remove the current-page class from all page buttons
+      pageButtons.forEach((button) => {
+        button.classList.remove('current-page');
+      });
+
+      // Add the current-page class to the first page button
+      document.querySelector('.pages-btn').classList.add('current-page');
+    }
+  });
+
+  // Event listener for last page button
+  document.querySelector('.last-page-btn').addEventListener('click', async () => {
+    const totalPages = await calculateTotalPages();
+    if (currentPage < totalPages) {
+      currentPage = totalPages;
       await updatePage();
     }
   });
@@ -110,11 +195,20 @@ export async function initializePagination() {
       if (!isNaN(pageNumber) && pageNumber !== currentPage) {
         currentPage = pageNumber;
         await updatePage();
+
+        // Remove the current-page class from all page buttons
+        pageButtons.forEach((button) => {
+          button.classList.remove('current-page');
+        });
+
+        // Add the current-page class to the clicked page button
+        btn.classList.add('current-page');
       }
     });
   });
 
   // Initial load
+  document.querySelector('.pages-btn:nth-child(1)').classList.add('current-page'); // Add the current-page class to the first page button
   await updatePageButtons();
   await updatePage();
 }
