c2113e54d8b4e83c6f06cc78881a8ce2310e5c2c
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-09-19 14:54:05 +0200

    update-js

--- src/js/actions/full-recipe.js
+++ src/js/actions/full-recipe.js
@@ -1,6 +1,5 @@
-import axios from 'axios';
 import { fetchRecipeByID } from '/js/API/recipe-id-api';
-// import { serviceAllRecipes } from '/js/API/recipe-api';
+import { getRecipeIdFromApi } from '/js/API/recipe-id-api';
 import { createMarkupModal } from '/js/markup/markup-full-recipe.js';
 
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
@@ -8,7 +7,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   allCards: document.querySelector('.js-card-list'), //що це? якщо список усіх карток, то там селектор js-card-list
-  allCardButtons: document.querySelectorAll('.card-btn'),
   modalCardCont: document.querySelector('.modal-card-markup'), // контейнер для відмальовки модалки
   modalBackdrop: document.querySelector('.modal-backdrop'), //бекдроп модального вікна
   modalButtonClose: document.querySelector('.modal-btn-close'),
@@ -17,77 +15,58 @@ const refs = {
   inputStar: document.querySelectorAll('.rating-star'), //зірки у модалці рейтингу
 };
 
-// refs.allCardButtons.addEventListener('click', handleCardButtonClick);
 refs.allCards.addEventListener('click', handlerGetIdCard);
-// refs.addToFavorite.addEventListener('click', addToLocalStorage);
 
 //функція відкриття модального вікна та забору id рецепту
 function handlerGetIdCard(evt) {
-  console.log(evt.target);
   const card = evt.target.closest('.card');
   Loading.standard('Loading...', { svgColor: '#9bb537' });
   const cardId = card.dataset.id;
-  console.log('Clicked card with _id:', cardId);
 
   fetchRecipeByID(cardId)
     .then(data => {
       const modalMarkup = createMarkupModal(data);
       refs.modalCardCont.innerHTML = modalMarkup;
-      // fillStars();
+      fillStars();
       openModal();
       Loading.remove();
     })
     .catch(error => {
+      console.error('Error fetching or rendering data:', error);
       Notify.failure(error.message);
+    })
+    .finally(() => {
+      refs.addToFavorite.addEventListener('click', addToLocalStorage);
     });
 }
 
-// Вызываем функцию для инициализации обработчиков клика
+function fillStars() {
+  const starRatings = document.querySelectorAll('.stars-block-js');
+  starRatings.forEach(starRating => {
+    //Отримую рейтинг(текст контент) з елемента
+    const rating = parseFloat(
+      starRating.querySelector('.cards-raiting').textContent
+    );
 
-// export function handlerGetIdCard(event) {
-//   if (event.target.classList.contains('card-btn')) {
-//     console.dir(event.target);
-// Loading.standard('Loading...', { svgColor: '#9bb537' });
+    //Округлюю до цілого числа
+    const roundedRating = Math.round(rating);
 
-// const buttonId = event.target.getAttribute('id');
-// refs.addToFavorite.id = buttonId;
-// const dataById = await fetchRecipeByID(buttonId);
-// const modalMarkup = createMarkupModal(dataById);
-// refs.modalCardCont.innerHTML = modalMarkup;
-// fillStars();
-// openModal();
-// Loading.remove();
-//   }
-// }
+    const stars = starRating.querySelectorAll('#all-stars');
 
-// function fillStars() {
-//   const starRatings = document.querySelectorAll('.card_star-rating');
-//   starRatings.forEach(starRating => {
-//     //Отримую рейтинг(текст контент) з елемента
-//     const rating = parseFloat(
-//       starRating.querySelector('.cards-raiting').textContent
-//     );
-
-//     //Округлюю до цілого числа
-//     const roundedRating = Math.round(rating);
-
-//     const stars = starRating.querySelectorAll('#all-stars');
-
-//     // Циклом по кожній зірці замальовую
-//     stars.forEach((star, index) => {
-//       if (index < roundedRating) {
-//         star.classList.add('js-stars');
-//       }
-//     });
-//   });
-// }
+    // Циклом по кожній зірці замальовую
+    stars.forEach((star, index) => {
+      if (index < roundedRating) {
+        star.classList.add('js-stars');
+      }
+    });
+  });
+}
 
 function openModal() {
-  console.log('открітие окна');
   refs.modalButtonClose.addEventListener('click', closeModal);
   refs.modalBackdrop.addEventListener('click', closeModalOnBackdrop);
   window.addEventListener('keydown', handleKeyDown);
-  refs.modalBackdrop.classList.add('is-open');
+  refs.modalBackdrop.classList.add('is-open-modal');
   document.body.style.overflow = 'hidden';
 }
 
@@ -101,7 +80,7 @@ function closeModal() {
   refs.modalButtonClose.removeEventListener('click', closeModal);
   refs.modalBackdrop.removeEventListener('click', closeModalOnBackdrop);
   window.removeEventListener('keydown', handleKeyDown);
-  refs.modalBackdrop.classList.remove('is-open');
+  refs.modalBackdrop.classList.remove('is-open-modal');
   document.body.style.overflow = 'auto';
   const youtubeIframe = document.querySelector('.iframe-video');
   youtubeIframe.src = '';
@@ -112,41 +91,42 @@ function closeModalOnBackdrop(event) {
     refs.modalButtonClose.removeEventListener('click', closeModal);
     refs.modalBackdrop.removeEventListener('click', closeModalOnBackdrop);
     window.removeEventListener('keydown', handleKeyDown);
-    refs.modalBackdrop.classList.remove('is-open');
+    refs.modalBackdrop.classList.remove('is-open-modal');
     document.body.style.overflow = 'auto';
     const youtubeIframe = document.querySelector('.iframe-video');
     youtubeIframe.src = '';
   }
 }
 
-// async function addToLocalStorage() {
-//   const addButtonId = addToFavorite.getAttribute('id');
-//   const recipeData = getRecipeIdFromApi(`/recipes/${addButtonId}`);
+function addToLocalStorage() {
+  console.log('клик');
+  // const addButtonId = addToFavorite.getAttribute('id');
+  // const recipeData = getRecipeIdFromApi(`/recipes/${addButtonId}`);
 
-//   const { category, description, preview, rating, title, _id } = recipeData;
+  // const { category, description, preview, rating, title, _id } = recipeData;
 
-//   const recipeObject = {
-//     category,
-//     description,
-//     preview,
-//     rating: rating.toFixed(1),
-//     title,
-//     _id,
-//   };
+  // const recipeObject = {
+  //   category,
+  //   description,
+  //   preview,
+  //   rating: rating.toFixed(1),
+  //   title,
+  //   _id,
+  // };
 
-//   let savedData = localStorage.getItem('localRecipes');
-//   savedData = savedData ? JSON.parse(savedData) : [];
+  // let savedData = localStorage.getItem('localRecipes');
+  // savedData = savedData ? JSON.parse(savedData) : [];
 
-//   const existingRecipeIndex = savedData.findIndex(data => data._id === _id);
+  // const existingRecipeIndex = savedData.findIndex(data => data._id === _id);
 
-//   if (existingRecipeIndex !== -1) {
-//     savedData.splice(existingRecipeIndex, 1);
-//     Notify.warning(`Recipe removed from local storage: ${recipeObject.title}`);
-//     addToFavorite.textContent = 'Add to favorite';
-//   } else {
-//     savedData.push(recipeObject);
-//     Notify.success(`Recipe added to local storage: ${recipeObject.title}`);
-//     addToFavorite.textContent = 'Remove favorite';
-//   }
-//   localStorage.setItem('localRecipes', JSON.stringify(savedData));
-// }
+  // if (existingRecipeIndex !== -1) {
+  //   savedData.splice(existingRecipeIndex, 1);
+  //   Notify.warning(`Recipe removed from local storage: ${recipeObject.title}`);
+  //   addToFavorite.textContent = 'Add to favorite';
+  // } else {
+  //   savedData.push(recipeObject);
+  //   Notify.success(`Recipe added to local storage: ${recipeObject.title}`);
+  //   addToFavorite.textContent = 'Remove favorite';
+  // }
+  // localStorage.setItem('localRecipes', JSON.stringify(savedData));
+}
--- src/js/markup/markup-full-recipe.js
+++ src/js/markup/markup-full-recipe.js
@@ -1,3 +1,5 @@
+import sprite from '../../img/icons.svg';
+
 export function createMarkupModal(data) {
   const youtubeLink = data.youtube;
 
@@ -33,54 +35,33 @@ export function createMarkupModal(data) {
     )
     .join('');
 
-  const modalCardMarkup = `
-          <iframe
-            src="${embedUrl}"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-            class="iframe-video"
-          ></iframe>
+  const modalCardMarkup = `<iframe
+  src="${embedUrl}"
+  title="YouTube video player"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  allowfullscreen
+  class="iframe-video"
+></iframe>
           <h2 class="modal-recipe-name">${data.title}</h2>
           <div class="modal-info">
-            <div class="modal-card-stars card_star-ratingg">
+            <div class="modal-card-stars stars-block-js">
               <p class="modal-stars-rating cards-raiting">${roundedRating}</p>
-              <div class="modal-stars rating-wrapper">
-                <svg class="card-stars-icon" data-raiting="one" id="all-stars">
-                  <path
-                    id="Star 1"
-                    d="M6.04894 1.42705C6.3483 0.505742 7.6517 0.505741 7.95106 1.42705L8.79611 4.02786C8.92999 4.43989 9.31394 4.71885 9.74717 4.71885H12.4818C13.4505 4.71885 13.8533 5.95846 13.0696 6.52786L10.8572 8.13525C10.5067 8.3899 10.3601 8.84127 10.494 9.25329L11.339 11.8541C11.6384 12.7754 10.5839 13.5415 9.80017 12.9721L7.58779 11.3647C7.2373 11.1101 6.7627 11.1101 6.41221 11.3647L4.19983 12.9721C3.41612 13.5415 2.36164 12.7754 2.66099 11.8541L3.50604 9.25329C3.63992 8.84127 3.49326 8.3899 3.14277 8.13525L0.930391 6.52787C0.146678 5.95846 0.549452 4.71885 1.51818 4.71885H4.25283C4.68606 4.71885 5.07001 4.43989 5.20389 4.02786L6.04894 1.42705Z"
-                    sroke="black"
-                  ></path>
+              <div class="modal-stars rating-wrapper ">
+                <svg class="card-stars-icon" data-raiting="one" id="all-stars" width="18" height="18">
+                <use href="${sprite}#star"></use>
                 </svg>
-                <svg class="card-stars-icon" data-raiting="two" id="all-stars">
-                  <path
-                    id="Star 1"
-                    d="M6.04894 1.42705C6.3483 0.505742 7.6517 0.505741 7.95106 1.42705L8.79611 4.02786C8.92999 4.43989 9.31394 4.71885 9.74717 4.71885H12.4818C13.4505 4.71885 13.8533 5.95846 13.0696 6.52786L10.8572 8.13525C10.5067 8.3899 10.3601 8.84127 10.494 9.25329L11.339 11.8541C11.6384 12.7754 10.5839 13.5415 9.80017 12.9721L7.58779 11.3647C7.2373 11.1101 6.7627 11.1101 6.41221 11.3647L4.19983 12.9721C3.41612 13.5415 2.36164 12.7754 2.66099 11.8541L3.50604 9.25329C3.63992 8.84127 3.49326 8.3899 3.14277 8.13525L0.930391 6.52787C0.146678 5.95846 0.549452 4.71885 1.51818 4.71885H4.25283C4.68606 4.71885 5.07001 4.43989 5.20389 4.02786L6.04894 1.42705Z"
-                    sroke="black"
-                  ></path>
+                <svg class="card-stars-icon" data-raiting="two" id="all-stars" width="18" height="18">
+                <use href="${sprite}#star"></use>
                 </svg>
-                <svg class="card-stars-icon" data-raiting="three" id="all-stars">
-                  <path
-                    id="Star 1"
-                    d="M6.04894 1.42705C6.3483 0.505742 7.6517 0.505741 7.95106 1.42705L8.79611 4.02786C8.92999 4.43989 9.31394 4.71885 9.74717 4.71885H12.4818C13.4505 4.71885 13.8533 5.95846 13.0696 6.52786L10.8572 8.13525C10.5067 8.3899 10.3601 8.84127 10.494 9.25329L11.339 11.8541C11.6384 12.7754 10.5839 13.5415 9.80017 12.9721L7.58779 11.3647C7.2373 11.1101 6.7627 11.1101 6.41221 11.3647L4.19983 12.9721C3.41612 13.5415 2.36164 12.7754 2.66099 11.8541L3.50604 9.25329C3.63992 8.84127 3.49326 8.3899 3.14277 8.13525L0.930391 6.52787C0.146678 5.95846 0.549452 4.71885 1.51818 4.71885H4.25283C4.68606 4.71885 5.07001 4.43989 5.20389 4.02786L6.04894 1.42705Z"
-                    sroke="black"
-                  ></path>
+                <svg class="card-stars-icon" data-raiting="three" id="all-stars" width="18" height="18">
+                <use href="${sprite}#star"></use>
                 </svg>
-                <svg class="card-stars-icon" data-raiting="four" id="all-stars">
-                  <path
-                    id="Star 1"
-                    d="M6.04894 1.42705C6.3483 0.505742 7.6517 0.505741 7.95106 1.42705L8.79611 4.02786C8.92999 4.43989 9.31394 4.71885 9.74717 4.71885H12.4818C13.4505 4.71885 13.8533 5.95846 13.0696 6.52786L10.8572 8.13525C10.5067 8.3899 10.3601 8.84127 10.494 9.25329L11.339 11.8541C11.6384 12.7754 10.5839 13.5415 9.80017 12.9721L7.58779 11.3647C7.2373 11.1101 6.7627 11.1101 6.41221 11.3647L4.19983 12.9721C3.41612 13.5415 2.36164 12.7754 2.66099 11.8541L3.50604 9.25329C3.63992 8.84127 3.49326 8.3899 3.14277 8.13525L0.930391 6.52787C0.146678 5.95846 0.549452 4.71885 1.51818 4.71885H4.25283C4.68606 4.71885 5.07001 4.43989 5.20389 4.02786L6.04894 1.42705Z"
-                    sroke="black"
-                  ></path>
+                <svg class="card-stars-icon" data-raiting="four" id="all-stars" width="18" height="18">
+                <use href="${sprite}#star"></use>
                 </svg>
-                <svg class="card-stars-icon" data-raiting="five" id="all-stars">
-                  <path
-                    id="Star 1"
-                    d="M6.04894 1.42705C6.3483 0.505742 7.6517 0.505741 7.95106 1.42705L8.79611 4.02786C8.92999 4.43989 9.31394 4.71885 9.74717 4.71885H12.4818C13.4505 4.71885 13.8533 5.95846 13.0696 6.52786L10.8572 8.13525C10.5067 8.3899 10.3601 8.84127 10.494 9.25329L11.339 11.8541C11.6384 12.7754 10.5839 13.5415 9.80017 12.9721L7.58779 11.3647C7.2373 11.1101 6.7627 11.1101 6.41221 11.3647L4.19983 12.9721C3.41612 13.5415 2.36164 12.7754 2.66099 11.8541L3.50604 9.25329C3.63992 8.84127 3.49326 8.3899 3.14277 8.13525L0.930391 6.52787C0.146678 5.95846 0.549452 4.71885 1.51818 4.71885H4.25283C4.68606 4.71885 5.07001 4.43989 5.20389 4.02786L6.04894 1.42705Z"
-                    sroke="black"
-                  ></path>
+                <svg class="card-stars-icon" data-raiting="five" id="all-stars" width="18" height="18">
+                <use href="${sprite}#star"></use>
                 </svg>
               </div>
               <p class="modal-card-time">${data.time} min</p>
@@ -89,6 +70,10 @@ export function createMarkupModal(data) {
             <ul class="modal-hashtags">${tagsMarkup}</ul>
             <p class="modal-recipe-instructions">${data.instructions}</p>
           </div>
+          <div class="modal-button">
+                <button class="modal-add-favorite btn">Add to favorite</button>
+                <button class="modal-give-rating btn">Give a rating</button>
+              </div>
     `;
 
   return modalCardMarkup;
