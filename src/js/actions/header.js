94fe3a2867b006e9922882886d2d8522c4a40ca7
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-09-19 17:44:49 +0200

    update-header

--- src/js/actions/header.js
+++ src/js/actions/header.js
@@ -1,17 +1,80 @@
 const openMobileMenuElement = document.querySelector('.burger-btn');
 const closeMobileMenuElement = document.querySelector('.mob-menu-close-btn');
-const mobileMenuElement = document.querySelector('.mobile-menu')
+const mobileMenuElement = document.querySelector('.mobile-menu');
+const menuLinkHomeElement = document.querySelector('.link-home');
+const menuLinkFavElement = document.querySelector('.link-fav');
+const mobMenuLinkHomeElement = document.querySelector('.mob-menu-link-home');
+const mobMenuLinkFavElement = document.querySelector('.mob-menu-link-fav');
 
-openMobileMenuElement.addEventListener('click', openMobileMenu)
-closeMobileMenuElement.addEventListener('click', closeMobileMenu)
+menuLinkFavElement.addEventListener('click', handlerMenuLink);
+mobMenuLinkFavElement.addEventListener('click', handlerMobMenuLink);
 
-export function openMobileMenu () {
-    mobileMenuElement.classList.add('is-open');
-    mobileMenuElement.classList.remove('visually-hidden');
+openMobileMenuElement.addEventListener('click', openMobileMenu);
+closeMobileMenuElement.addEventListener('click', closeMobileMenu);
+
+export function openMobileMenu() {
+  mobileMenuElement.classList.add('is-open');
+  mobileMenuElement.classList.remove('visually-hidden');
 }
 
-export function closeMobileMenu () {
-    mobileMenuElement.classList.remove('is-open');
-    mobileMenuElement.classList.add('visually-hidden');
+export function closeMobileMenu() {
+  mobileMenuElement.classList.remove('is-open');
+  mobileMenuElement.classList.add('visually-hidden');
 }
 
+export function handlerMenuLink() {
+  menuLinkFavElement.classList.add('current');
+  menuLinkHomeElement.classList.remove('current');
+}
+
+export function handlerMobMenuLink() {
+  mobMenuLinkFavElement.classList.add('active-mob-nav');
+  mobMenuLinkHomeElement.classList.remove('active-mob-nav');
+}
+
+//  =========== change the theme ===========
+
+window.addEventListener('load', windowLoad);
+
+export function windowLoad() {
+  const htmlBlock = document.documentElement;
+
+  const saveUserTheme = localStorage.getItem('user-theme');
+
+  const themeButton = document.querySelector('.switch__input');
+  const mobileThemeButton = document.querySelector('.switch__input__mob');
+  if (themeButton) {
+    themeButton.addEventListener('click', function (e) {
+      changeTheme(true);
+    });
+  }
+  if (mobileThemeButton) {
+    mobileThemeButton.addEventListener('click', function (e) {
+      changeTheme(true);
+    });
+  }
+
+  function setThemeClass() {
+    if (saveUserTheme) {
+      htmlBlock.classList.add(saveUserTheme);
+    } else {
+      htmlBlock.classList.add(userTheme);
+    }
+  }
+
+  setThemeClass();
+
+  function changeTheme(saveTheme = false) {
+    let currentTheme = htmlBlock.classList.contains('dark') ? 'dark' : 'light';
+    let newTheme;
+
+    if (currentTheme === 'light') {
+      newTheme = 'dark';
+    } else if (currentTheme === 'dark') {
+      newTheme = 'light';
+    }
+    htmlBlock.classList.remove(currentTheme);
+    htmlBlock.classList.add(newTheme);
+    saveTheme ? localStorage.setItem('user-theme', newTheme) : null;
+  }
+}
