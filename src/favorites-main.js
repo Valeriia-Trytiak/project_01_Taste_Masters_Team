f80cd5ce53e7af5687d5fe1ed5d61a98a56b0eed
Agmund2002 <malysh.andrey2000@gmail.com>
2023-09-20 17:39:16 +0300

    bugfix error

--- src/favorites-main.js
+++ src/favorites-main.js
@@ -1,6 +1,6 @@
 import { openMobileMenu } from '/js/actions/header';
 import { closeMobileMenu } from '/js/actions/header';
-import {} from '/js/actions/modal-new-order';
+import {} from '/js/actions/modal-new-order-fav';
 import { favoritesPage } from '/js/actions/favorites';
 
 favoritesPage();
