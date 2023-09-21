5f1f77a29c11dce816d5018f223fdc266c3c95d7
Agmund2002 <malysh.andrey2000@gmail.com>
2023-09-22 02:51:52 +0300

    add changes

--- src/favorites-main.js
+++ src/favorites-main.js
@@ -2,6 +2,5 @@ import { openMobileMenu } from '/js/actions/header';
 import { closeMobileMenu } from '/js/actions/header';
 import {} from '/js/actions/modal-new-order-fav';
 import { favoritesPage } from '/js/actions/favorites';
-// import {} from '/js/actions/full-recipe.js';
 
 favoritesPage();
