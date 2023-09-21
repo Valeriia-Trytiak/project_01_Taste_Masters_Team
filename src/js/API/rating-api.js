5503a5f710ab4a325906c64d776a43d993db7887
Cajamarquina <127974997+Cajamarquina@users.noreply.github.com>
2023-09-21 18:24:18 +0200

    Latest update rating-modal

--- src/js/API/rating-api.js
+++ src/js/API/rating-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Function to fetch the rating for a specific recipe by its ID
 export async function fetchRatingById(recipeId) {
   try {
-    const apiUrl = `https://tasty-treats-backend.p.goit.global/api/recipes/${Id}/rating`;
+    const apiUrl = `https://tasty-treats-backend.p.goit.global/api/recipes/${recipeId}/rating`;
     const response = await axios.get(apiUrl);
     return response.data.rating;
   } catch (error) {
@@ -13,7 +13,7 @@ export async function fetchRatingById(recipeId) {
 // Function to submit a rating for a specific recipe by its ID
 export async function rateRecipeById(recipeId, userRating, userEmail) {
   try {
-    const apiUrl = `https://tasty-treats-backend.p.goit.global/api/recipes/${Id}/rating`;
+    const apiUrl = `https://tasty-treats-backend.p.goit.global/api/recipes/${recipeId}/rating`;
     const response = await axios.patch(apiUrl, { rating: userRating, email: userEmail });
     return response.data;
   } catch (error) {
