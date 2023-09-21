4e40de17c3fc5f665f7b88335041fca8c15b4019
Agmund2002 <malysh.andrey2000@gmail.com>
2023-09-22 00:12:19 +0300

    fixed the styles

--- src/js/actions/search.js
+++ src/js/actions/search.js
@@ -55,6 +55,13 @@ refs.resetBtn.addEventListener('click', onClickResetButton);
 // }
 
 function onChangeSelectFilter() {
+  if (refs.inputSearch.value !== '') {
+    refs.inputSearch.nextElementSibling.style.fill = 'var(--accent-cl)';
+  } else {
+    refs.inputSearch.nextElementSibling.style.fill =
+      'var(--txt-cl-50-light-theme)';
+  }
+
   const gridBox = document.querySelector('.js-card-list');
   const favoritesArr = JSON.parse(localStorage.getItem('cardsArray')) || [];
   const formData = new FormData(refs.searchForm);
